dc7d8ef5db477386f2fdf91c4e40b4cf5918d25c
tigger <fronttigger@gmail.com>
2021-11-16 22:45:25 +0900

    [#8]: endsWith 정리

--- src/string/endsWith/index.ts
+++ src/string/endsWith/index.ts
@@ -1,10 +1,3 @@
-// - [x] 파라미터가 비어있을 때
-// value
-// - [x] value만 있을 때 -> false
-// target
-// - [x] target만 있을 때 value의 마지막과 일치하는지 확인
-// - [x] target과 position이 함께 있을 때 position 번째의 위치를 확인
-
 function endsWith(value?: string, target?: string, position?: number): boolean {
   if (
     !value ||
