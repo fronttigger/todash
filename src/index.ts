3e8b8c54ea8893bf9ff8cc643d2fba1e9436a829
tigger <fronttigger@gmail.com>
2021-11-16 21:32:50 +0900

    feat: split 테스트

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import capitalize from './string/capitalize/index';
+import split from './string/split/index';
+
+export  capitalize, split ;
--- src/string/split/index.ts
+++ src/string/split/index.ts
@@ -9,17 +9,12 @@ function split(
 
   let array: string[] = [];
 
-  // 1. separator
-  // - [x] limit가 없을 수 있다.
-  // - [x] limit가 있을 수 있다.
-  // 2. limit가 있냐 없냐
-  // - [ ] separator와 함께 있다.
-  // - [ ] limit가 있어도 separator는 없을 수 있다.
-
   if (separator && !limit) {
     array = value.split(separator);
   } else if (separator && limit) {
     array = value.split(separator).slice(0, limit);
+  } else if (!separator && limit) {
+    array = value.split('').slice(0, limit);
   } else if (!separator && !limit) {
     array = [value];
   }
