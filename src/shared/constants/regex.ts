a7f4e3394b7d847f95252b12c600ae5a8a96890a
tigger <fronttigger@gmail.com>
2021-11-29 23:19:01 +0900

    [#24]: toLower 구현 & 테스트

---
+++ src/shared/constants/regex.ts
@@ -0,0 +1,2 @@
+export const ALL_ALPHABET_REGEX = /^[a-zA-Z]*$/;
+export const LOWERCASE_ALPHABET_REGEX = /^[a-z]*$/;
--- test/string/toLower/index.test.ts
+++ test/string/toLower/index.test.ts
@@ -4,4 +4,16 @@ describe('toLower', () => {
   it('파라미터가 비어있을 때 빈 문자열이 반환된다.', () => {
     expect(toLower()).toEqual('');
   });
+
+  it('알파벳이 아닌 값과 함께 입력하면 알파벳만 소문자로 바꾸어 반환된다.', () => {
+    expect(toLower('--Foo-Bar--')).toEqual('--foo-bar--');
+  });
+
+  it('알파벳이 아닌 값과 함께 입력하면 알파벳만 소문자로 바꾸어 반환된다.', () => {
+    expect(toLower('__FOO_BAR__')).toEqual('__foo_bar__');
+  });
+
+  it('소문자만 입력할 시 동일한 문자열이 반환된다.', () => {
+    expect(toLower('foobar')).toEqual('foobar');
+  });
 });
