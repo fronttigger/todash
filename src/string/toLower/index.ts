4b00fe787d525eae358413f225e10189616442ea
tigger <fronttigger@gmail.com>
2021-11-29 23:19:01 +0900

    [#24]: toLower 구현 & 테스트

--- src/string/toLower/index.ts
+++ src/string/toLower/index.ts
@@ -1,5 +1,28 @@
+import {
+  ALL_ALPHABET_REGEX,
+  LOWERCASE_ALPHABET_REGEX,
+} from '../../shared/constants/regex';
+
 function toLower(value: string = ''): string {
-  return value;
+  if (!value) {
+    return '';
+  }
+
+  const toLoweredValueArray: string[] = [];
+  const valueArray = value.split('');
+
+  valueArray.forEach((value) => {
+    if (
+      ALL_ALPHABET_REGEX.test(value) &&
+      !LOWERCASE_ALPHABET_REGEX.test(value)
+    ) {
+      toLoweredValueArray.push(value.toLowerCase());
+    } else {
+      toLoweredValueArray.push(value);
+    }
+  });
+
+  return toLoweredValueArray.join('');
 }
 
 export default toLower;
